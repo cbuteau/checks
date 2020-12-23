1fb1afbfba2014f0113ea350cc9028a8ee487d0c
Christopher Buteau <cbuteau@gmail.com>
2020-12-23 15:43:04 -0500

    needs tests

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 
+function stringProp(params) {
+  let prop = params.prop;
+  switch (params.test) {
+    case 'array':
+      if (!Array.isArray(prop)) {
+        throw new Error(key + ' is NOT an Array');
+      }
+      break;
+    case 'string':
+      if (typeof prop !== 'string') {
+      }
+      throw new Error(key + ' is NOT a String');
+      break;
+    default:
+      if (!prop) {
+        throw new Error(key + ' does not exist');
+      }
+      break;  }
+}
+
+function work(params) {
+  let parameters = params.params;
+  let checks = params.checks;
+  let keys = Object.keys(parameters);
+  let checks = Object.keys(checks);
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    let prop = parameters[key];
+    if (checks.indexOf(key) !== -1) {
+      let test = checks[key];
+      let isString = typeof test === 'string';
+      if (isString) {
+        stringProp({
+          prop: prop,
+          test: test
+        });
+      } else {
+        let checkKeys = Object.keys(test);
+        for (let j = 0; i < checkKeys.length; j++) {
+          let checkKey = checkKeys[j];
+          work({
+            parameters: prop,
+            checks: test[checkKey]
+          });
+        }
+      }
+    }
+  }
+}
+
 function Checks() {}
 
 Checks.prototype = {
   validate: function(parameters, checks) {
-    let keys = Object.keys(parameters);
-    let checks = Object.keys(checks);
-    for (let i = 0; i < keys.length; i++) {
-      let key = keys[i];
-      let prop = parameters[key];
-      if (checks.indexOf(key) !== -1) {
-        let test = checks[key];
-        switch (test) {
-          case 'array':
-            if (!Array.isArray(prop)) {
-              throw new Error(key + ' is NOT an Array');
-            }
-            break;
-          case 'string':
-            if (typeof prop !== 'string') {
-            }
-            throw new Error(key + ' is NOT a String');
-            break;
-          default:
-            if (!prop) {
-              throw new Error(key + ' does not exist');
-            }
-            break;
-        }
-      }
-    }
+    work({
+      params: parameters,
+      checks: checks
+    });
   }
 }
 
